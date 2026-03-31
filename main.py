97643598fc9b493027c57b4c791292d6e484a61c
sejun5145 <jeongsejun5145@mju.ac.kr>
2026-03-31 06:33:23 +0000

    정세준 60261955 미션 후 제출

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # 파일이름 :
 # 작 성 자 :
+buket_list = []
+restaurant = input('맛집 리스트 입력: ')
+buket_list.append(restaurant)
+restaurant = input('맛집 리스트 입력: ')
+buket_list.append(restaurant)
+restaurant  = input('맛집 리스트 입력: ')
+buket_list.append(restaurant)
+
+
+
